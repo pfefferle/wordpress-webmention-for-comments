29285ceda2ffc0939870cff376b0c0d01e7601b8
pfefferle <pfefferle@gmail.com>
2014-06-28 19:24:19 +0200

    some nicer comment html

--- templates/comment.php
+++ templates/comment.php
@@ -5,34 +5,34 @@ $comment = get_comment($comment_id);
 ?><!DOCTYPE htmL>
 <html>
   <head>
-  <?php
-    wp_head();
-  ?>
+    <?php wp_head();?>
 
     <script type="text/javascript">
-    <!--
-    // redirect to comment-page and scroll to comment
-    window.location = "<?php echo get_permalink($comment->comment_post_ID) . '#comment-' . $comment_id; ?>";
-    //–>
+      <!--
+      // redirect to comment-page and scroll to comment
+      window.location = "<?php echo get_permalink($comment->comment_post_ID) . '#comment-' . $comment_id; ?>";
+      //–>
     </script>
   </head>
 
-  <body>
-    <article id="comment-<?php comment_ID(); ?>" class="h-comment h-as-comment h-entry">
-      <div class="e-content p-summary p-name"><?php comment_text(); ?></div>
+  <body <?php body_class(); ?>>
+    <div id="page">
+      <?php do_action( 'before' ); ?>
 
-      <footer class="entry-meta">
-        <address class="p-author h-card">
-          <?php echo get_avatar( $comment, 50 ); ?>
-          <?php printf( '<cite class="p-name">%s</cite>', get_comment_author_link() ); ?>
-        </address><!-- .comment-author .vcard -->
-
-        <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time datetime="<?php comment_time( 'c' ); ?>" class="dt-published dt-updated published updated">
-        <?php
-          /* translators: 1: date, 2: time */
-          printf( __( '%1$s at %2$s', 'webmention' ), get_comment_date(), get_comment_time() ); ?>
-        </time></a>
-      </footer>
-    </article><!-- #comment-## -->
-  </body><!-- #primary -->
+      <article id="comment-<?php comment_ID(); ?>" class="h-comment h-as-comment h-entry">
+        <div class="e-content p-summary p-name"><?php comment_text(); ?></div>
+        <footer class="entry-meta">
+          <address class="p-author h-card">
+            <?php echo get_avatar( $comment, 50 ); ?>
+            <?php printf( '<cite class="p-name">%s</cite>', get_comment_author_link() ); ?>
+          </address><!-- .comment-author .vcard -->
+          <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time datetime="<?php comment_time( 'c' ); ?>" class="dt-published dt-updated published updated">
+            <?php
+            /* translators: 1: date, 2: time */
+            printf( __( '%1$s at %2$s', 'webmention' ), get_comment_date(), get_comment_time() ); ?>
+          </time></a>
+        </footer>
+      </article>
+    </div>
+  </body>
 </html>
