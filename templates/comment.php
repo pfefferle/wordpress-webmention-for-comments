7b83a289d96d0b73e30250433c3b6148a59a6a20
pfefferle <pfefferle@gmail.com>
2015-04-25 11:09:34 +0200

    fixed template
    added „in-reply-to“ link

--- templates/comment.php
+++ templates/comment.php
@@ -2,6 +2,15 @@
 global $wp_query, $post;
 $comment_id = $wp_query->query['replytocom'];
 $comment = get_comment($comment_id);
+$target = null;
+
+// check parent comment
+if ($comment->comment_parent) {
+  // get parent comment...
+  $parent = get_comment($comment->comment_parent);
+  // ...and gernerate target url
+  $target = $parent->comment_author_url;
+}
 ?><!DOCTYPE htmL>
 <html>
   <head>
@@ -15,22 +24,27 @@ $comment = get_comment($comment_id);
     </script>
   </head>
 
-  <body <?php body_class(); ?>>
+  <body <?php body_class("h-comment h-as-comment h-entry"); ?>>
     <div id="page">
-      <?php do_action( 'before' ); ?>
+      <?php do_action('before'); ?>
 
-      <article id="comment-<?php comment_ID(); ?>" class="h-comment h-as-comment h-entry">
+      <article id="comment-<?php comment_ID(); ?>">
         <div class="e-content p-summary p-name"><?php comment_text(); ?></div>
         <footer class="entry-meta">
           <address class="p-author h-card">
-            <?php echo get_avatar( $comment, 50 ); ?>
-            <?php printf( '<cite class="p-name">%s</cite>', get_comment_author_link() ); ?>
+            <?php echo get_avatar($comment, 50); ?>
+            <?php printf('<cite class="p-name">%s</cite>', get_comment_author_link()); ?>
           </address><!-- .comment-author .vcard -->
-          <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time datetime="<?php comment_time( 'c' ); ?>" class="dt-published dt-updated published updated">
+          <a href="<?php echo esc_url(get_comment_link($comment->comment_ID)); ?>"><time datetime="<?php comment_time('c'); ?>" class="dt-published dt-updated published updated">
             <?php
             /* translators: 1: date, 2: time */
-            printf( __( '%1$s at %2$s', 'webmention' ), get_comment_date(), get_comment_time() ); ?>
+            printf(__('%1$s at %2$s', 'webmention'), get_comment_date(), get_comment_time()); ?>
           </time></a>
+          <ul>
+          <?php if ($target) { ?>
+            <li><a href="<?php echo $target; ?>" rel="in-reply-to" class="u-in-reply-to"><?php echo $target; ?></a></li>
+          <?php } ?>
+          </ul>
         </footer>
       </article>
     </div>
